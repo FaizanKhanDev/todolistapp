c1b425791460c35f5216390147faf6b512433cda
FaizanKhanDev <faizan480khan@gmail.com>
2023-04-07 20:13:44 +0500

    First Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react'
+
+export default class App extends Component {
+  render() {
+    return (
+   <div className="container-fluid my-5">
+      <div className="row">
+        <div className="col-sm-6 text-white shadow-lg p-3  mx-auto">
+        <h1 className='text-center text-white'>Today's Plan</h1>
+        </div>
+
+      </div>
+   </div>
+    )
+  }
 }
 
-export default App;
+
+
